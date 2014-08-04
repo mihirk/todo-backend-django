3e2f84492456f1920194485eaa9ddaf9bcc5b1a3
mihirk <mihir.khatwani@gmail.com>
2014-08-04 18:37:01 +0530

    Added support for order

--- todo_backend_django/models.py
+++ todo_backend_django/models.py
@@ -5,3 +5,4 @@ class TodoItem(models.Model):
     title = models.CharField(max_length=256, null=True, blank=True)
     completed = models.NullBooleanField(null=True, blank=True, default=False)
     url = models.CharField(max_length=256, null=True, blank=True)
+    order = models.IntegerField(null=True, blank=True)
--- todo_backend_django/serializers.py
+++ todo_backend_django/serializers.py
@@ -7,15 +7,17 @@ class TodoItemSerializer(serializers.ModelSerializer):
     title = serializers.CharField(max_length=256, required=False, blank=True)
     completed = serializers.BooleanField(required=False, default=False)
     url = serializers.CharField(max_length=256, required=False, blank=True)
+    order = serializers.IntegerField(required=False, blank=True)
 
     def restore_object(self, attrs, instance=None):
         if instance:
             instance.title = attrs.get('title', instance.title)
             instance.completed = attrs.get('completed', instance.completed)
             instance.url = attrs.get('url', instance.url)
+            instance.order = attrs.get('order', instance.order)
             return instance
         return TodoItem(**attrs)
 
     class Meta:
         model = TodoItem
-        fields = ('title', 'completed', 'url')
+        fields = ('title', 'completed', 'url', 'order')
