4b9b9f60046a40f5d191c5f974ffddf08efdd8d7
mihirk <mihir.khatwani@gmail.com>
2014-08-04 17:14:54 +0530

    Making completed default to null

--- todo_backend_django/models.py
+++ todo_backend_django/models.py
@@ -3,4 +3,4 @@ from django.db import models
 
 class TodoItem(models.Model):
     title = models.CharField(max_length=256, null=True, blank=True)
-    completed = models.NullBooleanField(null=True, blank=True, default=None)
+    completed = models.NullBooleanField(null=True, blank=True, default=False)
--- todo_backend_django/serializers.py
+++ todo_backend_django/serializers.py
@@ -5,7 +5,7 @@ from todo_backend_django.models import TodoItem
 class TodoItemSerializer(serializers.ModelSerializer):
     pk = serializers.Field()
     title = serializers.CharField(max_length=256, required=False, blank=True)
-    completed = serializers.BooleanField(required=False, default=None)
+    completed = serializers.BooleanField(required=False, default=False)
 
     def restore_object(self, attrs, instance=None):
         if instance:
